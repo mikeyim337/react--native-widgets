12e03f8d8276e47216b63dfa06021814d49439c5
mikeyim337 <mk337337@gmail.com>
2021-04-20 01:33:18 -0700

    done with basic react stuff

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
 
+
 const HomeScreen = () => {
   return <Text style={styles.text}>Hi there!</Text>;
+    
+    
 };
 
 const styles = StyleSheet.create({
