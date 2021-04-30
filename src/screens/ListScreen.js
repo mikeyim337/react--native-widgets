bfa6917582dbe420a1b9c7b9c5ae68bba779f803
mikeyim337 <mk337337@gmail.com>
2021-04-30 03:27:15 -0700

    list building done

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
-    return <Text>List Screen</Text>;
+    const friends = [
+        { name: 'Freind #1', age: 20},
+        { name: 'Freind #2', age: 22},
+        { name: 'Freind #3', age: 26},
+        { name: 'Freind #4', age: 24},
+        { name: 'Freind #5', age: 22},
+        { name: 'Freind #6', age: 20},   
+
+    ]
+    return (
+        <FlatList
+            keyExtractor={(friend) => friend.name} 
+            data={friends} 
+            renderItem={({ item }) => {
+                return <Text style={styles.textStyle}>{ item.name } - Age { item.age }</Text>
+            }} 
+        />
+    );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    textStyle: {
+        marginVertical: 50
+    }
+});
 
 export default ListScreen;
